bc831fb990a2e6d651c9a32483bae3942c0d8063
apahadi73 <apahadi73@gmail.com>
2021-11-06 20:05:23 -0500

    index.js refactored

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 import http from "http";
 import nodeCron from "node-cron";
-import AppManager from "./container/AppManager.js";
+import AppManager from "./Container/AppManager.js";
 import chalk from "chalk";
+import WebSocketWrapper from "./websocket/WebSocketWrapper.js";
 
 dotenv.config();
 
@@ -11,10 +12,8 @@ let appManager = AppManager();
 let app = appManager.App;
 
 const server = http.Server(app);
-
 // sets port and listener
 const PORT = process.env.PORT || 5001;
-
 // only listen if not in test environment
 server.listen(PORT, async () => {
 	console.log(
@@ -26,9 +25,21 @@ server.listen(PORT, async () => {
 	if (process.env.NODE_ENV !== "test") {
 		await appManager.Migrate;
 		await appManager.Seed;
+		const dateTime = new Date().toLocaleString().split("/");
+		const scheduleTime = `${dateTime[2].split(",")[0]}-${dateTime[0]}-${
+			dateTime[1]
+		}`;
+
+		await appManager.EventScrapper;
+
+		const job = nodeCron.schedule("0 12 * * *", async () => {
+			await crawlEvents(scheduleTime);
+		});
+		job.start();
 	}
 });
 
 // wraps our server application
+WebSocketWrapper(server, appManager.MessageRepo);
 
 export default app;
