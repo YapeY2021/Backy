4c0fa16fd430741452cd175a8d4bedd77efd9f84
Amir Pahadi <apahadi73@gmail.com>
2021-11-20 22:11:51 -0600

    index.js restored

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
+import express from "express";
 import dotenv from "dotenv";
-import http from "http";
+import colors from "colors";
+import morgan from "morgan";
 import nodeCron from "node-cron";
-import AppManager from "./container/AppManager.js";
-import chalk from "chalk";
+import crawlEvents from "./scripts/crawlEvent.js";
+import http from "http";
 
+// routes import
+import { userRouter } from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import { eventRouter } from "./routes/eventRoutes.js";
+import { migrate } from "./scripts/migrate.js";
+import { seed } from "./scripts/seed.js";
+import app from "./app.js";
+
+// configures environment variables
+// we use this to inject the environment variables into our application
 dotenv.config();
 
-let appManager = AppManager();
-
-let app = appManager.App;
-
 const server = http.Server(app);
-
 // sets port and listener
-const PORT = process.env.PORT || 5001;
-
+const PORT = process.env.PORT || 5000;
 // only listen if not in test environment
 server.listen(PORT, async () => {
 	console.log(
-		chalk.blue.bold(
-			`------------------------Server is listening on port: ${process.env.PORT}!--------------------`
-		)
+		`Server is listening on port: ${process.env.PORT}!`.yellow.bold
 	);
-
+	await migrate();
+	await seed();
 	if (process.env.NODE_ENV !== "test") {
-		await appManager.Migrate;
-		await appManager.Seed;
+		const dateTime = new Date().toLocaleString().split("/");
+		const scheduleTime = `${dateTime[2].split(",")[0]}-${dateTime[0]}-${
+			dateTime[1]
+		}`;
+
+		await crawlEvents(scheduleTime);
+
+		const job = nodeCron.schedule("0 12 * * *", async () => {
+			await crawlEvents(scheduleTime);
+		});
+		job.start();
 	}
 });
-
-// wraps our server application
 
 export default app;
